360cacd226650cbe99eff6597d024f96b3d1571e
Tanay Nistala <t.nistala@icloud.com>
2023-02-17 16:29:35 +0000

    Added “Gallery” to page title

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Tanay Nistala</title>
+        <title>Tanay Nistala ∙ Gallery</title>
         <meta name="description" content="Photo gallery" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
