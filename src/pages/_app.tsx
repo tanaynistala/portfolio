d2563552da078f24df58527d289a9fe3fa122aba
Tanay Nistala <t.nistala@icloud.com>
2023-02-17 03:08:39 +0000

    Set up a simple site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import { Manrope, Inter, Work_Sans } from "@next/font/google"
+
+const manrope = Manrope({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] })
+const work = Work_Sans({ subsets: ["latin"] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main>
+      <Component {...pageProps} />
+    </main>
+  )
 }
