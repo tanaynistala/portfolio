e2d71a3ffc52df007dbaec268b1809a16a0e4df7
Tanay Nistala <t.nistala@icloud.com>
2023-03-10 14:12:20 -0500

    Changed "show more" button styling

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,19 +1,18 @@
-import { useState } from "react"
-import Head from "next/head"
-import Image from "next/image"
-import Link from "next/link"
-import photos from "@/pages/api/photos.json"
+import { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import photos from "@/pages/api/photos.json";
 
 export type Photo = {
-  id: number
-  title: string
-  location: string
-  date: string
-}
+  id: number;
+  title: string;
+  location: string;
+  date: string;
+};
 
 export default function Home() {
-
-  const [ numVisiblePhotos, showPhotos ] = useState(10);
+  const [numVisiblePhotos, showPhotos] = useState(10);
 
   return (
     <>
@@ -65,11 +64,11 @@ export default function Home() {
         <div className="mt-8">
           {photos
             .sort((a, b) => {
-              return Date.parse(b.date) - Date.parse(a.date)
+              return Date.parse(b.date) - Date.parse(a.date);
             })
             .slice(0, numVisiblePhotos)
             .map((photo) => {
-              const date = new Date(photo.date)
+              const date = new Date(photo.date);
               return (
                 <div key={photo.id}>
                   <Link href={`photos/photo${photo.id}.png`}>
@@ -92,16 +91,39 @@ export default function Home() {
                     </p>
                   </Link>
                 </div>
-              )
+              );
             })}
 
-          <button className="font-medium opacity-50 hover:opacity-100 w-full mx-auto mt-8" onClick={() => {
-            showPhotos(Math.min(numVisiblePhotos+10, photos.length))
-          }}>
-            Show More
-          </button>
+          <div className="w-full mt-8 flex justify-center">
+            {numVisiblePhotos < photos.length && (
+              <button
+                className="opacity-50 hover:opacity-100 flex flex-col items-center"
+                onClick={() => {
+                  showPhotos(Math.min(numVisiblePhotos + 10, photos.length));
+                }}
+              >
+                Show More
+                <div>
+                  <svg
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M7.5 2C7.77614 2 8 2.22386 8 2.5L8 11.2929L11.1464 8.14645C11.3417 7.95118 11.6583 7.95118 11.8536 8.14645C12.0488 8.34171 12.0488 8.65829 11.8536 8.85355L7.85355 12.8536C7.75979 12.9473 7.63261 13 7.5 13C7.36739 13 7.24021 12.9473 7.14645 12.8536L3.14645 8.85355C2.95118 8.65829 2.95118 8.34171 3.14645 8.14645C3.34171 7.95118 3.65829 7.95118 3.85355 8.14645L7 11.2929L7 2.5C7 2.22386 7.22386 2 7.5 2Z"
+                      fill="currentColor"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </button>
+            )}
+          </div>
         </div>
       </main>
     </>
-  )
+  );
 }
